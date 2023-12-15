79c74cb01bff337124e8c630c55d31af2e91d014
GahiwalTejas <tejasgahiwal1999@gmail.com>
2023-12-15 20:02:48 +0530

    database Service

---
+++ SmartStudyNotes/src/appwrite/databaseService.js
@@ -0,0 +1,133 @@
+import { Client, Databases,Query } from "appwrite";
+import config from "../config/config";
+
+export class DatabaseService{
+     client = new Client();
+     databases;
+
+constructor (){
+   this.client
+    .setEndpoint(config.appwriteUrl)
+    .setProject(config.appwriteProjectId);
+
+
+     databases=new  Databases(this.client);
+
+}
+
+async createPost({title,slug,content,featuredImage,status,userId}){
+  try {
+    return await this.databases.createDocument(
+      config.appwriteDatabaseId,
+      config.appwriteCollectionId,
+      slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId
+        }
+        
+        )
+    
+  } catch (error) {
+    console.log("AppwriteService :createPost::error->"+error);
+  }
+
+}
+
+
+async updatePost(slug,{title,content,featuredImage,status}){
+  try {
+    return await this.databases.updateDocument(
+      config.appwriteDatabaseId,
+      config.appwriteCollectionId,
+      slug,
+      {
+        title,
+        content,
+        featuredImage,
+        status
+      }
+
+    )
+    
+  } catch (error) {
+    console.log("AppwriteService :updatePost::error->"+error);
+
+  }
+
+
+
+}
+
+
+async deletePost(slug){
+ try {
+   await this.databases.deleteDocument(
+    config.appwriteDatabaseId,
+    config.appwriteCollectionId,
+    slug
+  )
+
+  return true;
+  
+ } catch (error) {
+  console.log("AppwriteService :deletePost::error->"+error);
+  return false;
+
+ }
+
+
+}
+
+
+  async getPost(slug){
+    try {
+     return await this.databases.getDocument(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        slug
+      )
+      
+    } catch (error) {
+      console.log("AppwriteService :deletePost::error->"+error);
+           return false;
+    }
+           
+
+  }
+
+   async getPosts(){
+           try {
+           
+  return await this.databases.listDocuments(
+    config.appwriteDatabaseId,
+    config.appwriteCollectionId,
+     [
+      Query.equal('status', ['active'])
+     ] 
+     )
+
+        } catch (error) {
+            console.log("AppwriteService :deletePost::error->"+error);
+            return false;
+           }
+
+
+   }
+
+
+
+
+
+
+
+
+}
+
+
+const databaseService=new DatabaseService();
+
+export default databaseService;
