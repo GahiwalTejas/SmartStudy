abecf25b3a741b6ffe2f0256fffcfbf02a826f26
GahiwalTejas <tejasgahiwal1999@gmail.com>
2023-12-20 19:09:20 +0530

    Header Compo. added

--- SmartStudyNotes/src/components/Header/Header.jsx
+++ SmartStudyNotes/src/components/Header/Header.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import {Container,Logo,LogOutBtn} from '../index'
+import { Link } from 'react-router-dom'
+import  {useSelector} from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 
 function Header() {
   return (
